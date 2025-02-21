1990e893a603e21b5e46cdb1d61d70d8a13e865a
gdany <dany.ghassan@ynov.com>
2025-02-21 11:09:09 +0100

    /articles squelette fini

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,10 +13,13 @@ final class ArticlesController extends AbstractController
 {
 
     #[Route('/articles', name: 'app_articles')]
-    public function index(): Response
+    public function show_products(EntityManagerInterface $entityManager): Response
     {
+
+        $articles = $entityManager->getRepository(Article::class)->findAll();
+
         return $this->render('articles/index.html.twig', [
-            'controller_name' => 'ArticlesController',
+            'articles' => $articles,
         ]);
     }
 }
